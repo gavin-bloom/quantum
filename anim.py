c191b374b3a50d4bd87883d84b42b8fa399d7faa
gavin <gavin@qm9.govereau.com>
2021-04-26 18:23:36 -0400

    te

--- anim.py
+++ anim.py
@@ -5,23 +5,12 @@ from qutip import *
 from matplotlib import cm
 import imageio
 
-x = 1 / (np.sqrt(2))
-E = 3
-
-def farhi_gutman(x,E,t):
-    return (np.exp(E*-1j*t)*((x*cos(E*x*t)- 1j*sin(E*x*t))*basis(2,0) + x*cos(E*x*t)*basis(2,1)))
-
-def scale_t(x,E,t):
-    return t * pi * (1/x) * (1/(2*E*10))
-
 def animate_bloch(states, duration=0.05, save_all=False):
 
     b = Bloch()
     b.xlabel = ['$\\left|+\\right>$', '$\\left|-\\right>$']
     b.ylabel = ['$\\left|i\\right>$','$\\left|-i\\right>$']
     b.zlabel = ['$\\left|0\\right>$', '$\\left|1\\right>$']
-    
-    b.add_states([farhi_gutman(x,E,0),farhi_gutman(x,E,scale_t(x,E,10))])
 
     b.vector_color = ['r']
     b.view = [-40,30]
@@ -53,10 +42,19 @@ def animate_bloch(states, duration=0.05, save_all=False):
         images.append(imageio.imread(filename))
     imageio.mimsave('bloch_anim.gif', images, duration=duration)
 
+def farhi_gutman(x,E,t):
+    return (np.exp(E*-1j*t)*((x*cos(E*x*t)- 1j*sin(E*x*t))*basis(2,0) + x*cos(E*x*t)*basis(2,1)))
+
+def scale_t(x,E,t):
+    return t * pi * (1/x) * (1/(2*E*10))
+
 def main():
     states = []
+    x = 1 / (np.sqrt(2))
+    E = 3
     #ts = linspace(0,(pi*x*(1/30)),int((pi*x*(1/3))))
     ts = linspace(0,1,10)
+    #b.add_states([farhi_gutman(x,E,0),farhi_gutman(x,E,scale_t(x,E,10))])
     for t in ts:
         t = scale_t(x,E,t)
         states.append(farhi_gutman(x,E,t).unit())
